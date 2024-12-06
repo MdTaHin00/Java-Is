456a195a947bb58b62a973d0f6db842b9c2f4ee3
tahin <mdtahinhassanjihad@gmail.com>
2024-12-06 10:23:25 +0600

    infex file & apps file

---
+++ apps.js
@@ -0,0 +1,31 @@
+// Basic Questions related to Variables Solve:
+
+// 1
+let myName="Tahin!";
+console.log("Hello,", myName);
+
+// 2
+let mathsNumbar=20;
+let mathsNumber2=25;
+console.log(mathsNumbar+mathsNumber2);
+
+// 3
+let personal="I am ";
+let age=20;
+let personal2="years old"
+console.log(personal,age,personal2);
+
+// 4
+let Maths=70;
+let English=80;
+let Physics=85;
+let tatalMarka=Maths+English+Physics;
+console.log(tatalMarka/3);
+
+// 5
+
+let myRoll=101421;
+let myroll=101421;
+// My Details
+
+// Basic Questions related to String Methods
